4811150ace2c743574e9bc8ded4f123ebadd2011
bala545 <reddy.balu515@gmail.com>
2024-02-08 06:03:06 +0530

    ignite changes

--- App.js
+++ App.js
@@ -7,23 +7,27 @@
  * 
  * ReactElement(object)  => HTML(BROWSER understands)
  */
+import React from "react";
+import ReactDOM  from "react-dom/client";
 
 const parent = React.createElement('div',
-{id:"parent"},
-[React.createElement('div',{id:"child"},
-[React.createElement('h1',{id:"h1Tag"},"I'm an h1 tag"),
-React.createElement('h2',{id:"h2Tag"},"I'm an h2 tag")] // here we will get an error each props should unique key
-),React.createElement('div',{id:"child2"},
-[React.createElement('h1',{id:"h1Tag"},"I'm an h1 tag"),
-React.createElement('h2',{id:"h2Tag"},"I'm an h2 tag")] // here we will get an error each props should unique key
-)]
+    { id: "parent" },
+    [React.createElement('div', { id: "child" },
+        [React.createElement('h1', { id: "h1Tag" }, "Hi This bala"),
+        React.createElement('h2', { id: "h2Tag" }, "I'm an h2 tag")] // here we will get an error each props should unique key
+    ), React.createElement('div', { id: "child2" },
+        [React.createElement('h1', { id: "h1Tag1" }, "I'm an h1 tag"),
+        React.createElement('h2', { id: "h2Tag2" }, "I'm an h2 tag")] // here we will get an error each props should unique key
+    )]
 );
+
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
 
 // const heading = React.createElement(
 //     'h1', {id:"heading",xyz:"abc"},
 //      "Hello World From React!"
 // );
 // console.log(heading); //object
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-#Hello React
+# Hello React
+
+# parcel 
+- Dev Build
+- Local Server
+- HMR  HotModule Replacement
+-File watchng algorithem Parcel means written in C++
+-caching Faster Builds
+-Image Optimization --costly thing
+-minfy our files in Prod,it bundles
+-compress files
+-consistent hashing
+-code splitting
+-diffrent bundling -- Support Older Browsers
+-diagnostics -- better error suggestion
+-https 
+-Tree Shaking -- remove unused code for us
+-lazy mode
